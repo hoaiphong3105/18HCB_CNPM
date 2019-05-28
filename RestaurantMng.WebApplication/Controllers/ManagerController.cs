85dd5cd6ec3c875fb7952289fa161e4bb4ec13ef
vominhduy <vominhduy95@gmail.com>
2019-05-28 14:33:35 +0700

    Add menu for each role

---
+++ RestaurantMng.WebApplication/Controllers/ManagerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantMng.WebApplication.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            return PartialView("PVTopGiaCao");
+        }
+    }
+}
